71e6426940202bef7904d1fcdf72898b1f36fde3
Ethan Harig <eharig09@gmail.com>
2026-08-28 06:52:13 -0400

    Enable schedule noon display correction

--- sports_aggregator/cfb/__init__.py
+++ sports_aggregator/cfb/__init__.py
@@ -3,6 +3,9 @@
 from sports_aggregator.cfb.cfbd import CFBDClient, CFBDConfigurationError, CFBDRequestError
 from sports_aggregator.cfb.repository import CFBRepository
 from sports_aggregator.cfb.sync import CFBDataSync
+from sports_aggregator.cfb.schedule_time_display import install_schedule_noon_display_fix
+
+install_schedule_noon_display_fix()
 
 __all__ = [
     "CFBDClient",
