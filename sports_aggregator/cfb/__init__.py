1eb0035579155e9e0e9d62120c282c256a30136f
Ethan Harig <eharig09@gmail.com>
2026-08-29 11:29:27 -0400

    Install team leader roster scope guard

--- sports_aggregator/cfb/__init__.py
+++ sports_aggregator/cfb/__init__.py
@@ -4,8 +4,10 @@ from sports_aggregator.cfb.cfbd import CFBDClient, CFBDConfigurationError, CFBDR
 from sports_aggregator.cfb.repository import CFBRepository
 from sports_aggregator.cfb.sync import CFBDataSync
 from sports_aggregator.cfb.schedule_time_display import install_schedule_noon_display_fix
+from sports_aggregator.cfb.leader_scope import install_team_leader_scope_guard
 
 install_schedule_noon_display_fix()
+install_team_leader_scope_guard(CFBRepository)
 
 __all__ = [
     "CFBDClient",
